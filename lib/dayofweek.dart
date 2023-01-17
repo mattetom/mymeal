ee207dd5d5e9b3982fedd0a31a4d606616c72c1c
Matteo Tomasini <matteo.tomasini@dataexpert.it>
2023-01-17 10:02:54 +0100

    Removed log.

--- lib/dayofweek.dart
+++ lib/dayofweek.dart
@@ -44,7 +44,6 @@ class DayOfWeekPage extends StatelessWidget {
                         value: launch,
                       ),
                     );
-                    log(value);
                   },
                 ),
               ),
