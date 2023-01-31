1c42ed5b3c15ade8a17b54450624186824aa0e85
Matteo Tomasini <matteo.tomasini@dataexpert.it>
2023-01-31 17:38:50 +0100

    Adding implementation of grocerylist.

--- lib/grocery_list.dart
+++ lib/grocery_list.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:menu/applicationstate.dart';
+import 'package:provider/provider.dart';
 
 /// Widget for the root/initial pages in the bottom navigation bar.
 class GroceryListPage extends StatelessWidget {
@@ -11,15 +14,44 @@ class GroceryListPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Grocery List'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text('Screen Grocery List',
-                style: Theme.of(context).textTheme.titleLarge),
-            const Padding(padding: EdgeInsets.all(4)),
-          ],
-        ),
+      body: FutureBuilder(
+        future: FirebaseFirestore.instance
+            .collection("groceryList")
+            .where('family',
+                isEqualTo: Provider.of<ApplicationState>(context).family.id)
+            .get(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          } else {
+            return StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection(
+                        '${snapshot.data?.docs.first.reference.path}/groceryListItems')
+                    .snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  }
+                  final groceryListItems = snapshot.data?.docs;
+                  if (groceryListItems!.isEmpty) {
+                    return const Text("Your grocery list is empty!");
+                  }
+                  return ListView.builder(
+                      itemCount: groceryListItems.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: Checkbox(
+                            value: groceryListItems[index]["checked"],
+                            onChanged: (bool? value) {},
+                          ),
+                          title: Text(groceryListItems[index]["value"]),
+                        );
+                      });
+                });
+          }
+        },
       ),
     );
   }
